01383f19ff91b3a80f1aa93d9ebf83a0760951da
Regiane Heil Plem <110128888+RegianePlem@users.noreply.github.com>
2024-11-14 20:54:13 -0300

    ...

--- Graficos/quantidadeusuarios.js
+++ Graficos/quantidadeusuarios.js
@@ -2,4 +2,22 @@ async function quantidadeusuarios() {
 const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/esportes/esportes-mais-praticados.json'
 const res = await fetch (url)
 const dados = await res.json ()
+const nomedasredes = Object.keys (dados)
+const quantidadeusuarios = Object.values (dados)
+const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeUsuarios,
+      type: 'bar'
+    }
+  ]
+
+  const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+
 }
+
+quantidadeUsuarios()
+
